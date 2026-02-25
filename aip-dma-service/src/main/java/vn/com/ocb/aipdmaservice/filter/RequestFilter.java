25cdd98d7d7ed491946f683bef7e0105d9d51ab0
HDBANK\thinhdc1 <thinhdc1>
2026-02-25 15:09:51 +0700

    Update

---
+++ aip-dma-service/src/main/java/vn/com/ocb/aipdmaservice/filter/RequestFilter.java
@@ -0,0 +1,27 @@
+package vn.com.ocb.aipdmaservice.filter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class RequestFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        String path = httpServletRequest.getServletPath();
+        log.info("Request url=[{}]", path);
+        chain.doFilter(request, response);
+        return;
+    }
+}
